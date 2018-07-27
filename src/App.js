2f7bd99e27ddfc102abb6872b89ee4762843b733
breakingBrad <bradvanorman@gmail.com>
2018-07-26 19:36:42 -0600

    step 1, part 1 - routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Auth from './components/Auth/Auth';
-import Dashboard from './components/Dashboard/Dashboard';
-import Form from './components/Form/Form';
+// import {Route} from 'react-router-dom'
 import Nav from './components/Nav/Nav';
-import Post from './components/Post/Post';
+import routes from './routes'
 
 class App extends Component {
   render() {
+    // const nav = this.props.location.pathname;
     return (
       <div className="App">
-        <Auth/>
-        <Dashboard/>
-        <Form/>
-        <Nav/>
-        <Post/>
+       <Nav
+       />
+        {routes}
       </div>
     );
   }
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import { withRouter, Link } from 'react-router-dom';
+
 
 class Nav extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
   render() {
-    return(
-    <div>
-      Nav
-    </div>
-    )
+    const { location, /*router, params, routes */ } = this.props;
+    if (location.pathname !== '/') {
+      console.log('nav', location.pathname)
+      return (
+        <div className="nav-container">
+          Nav:
+          <div className="nav-button-container">
+          <Link to="/dashboard">
+            <button className="home-button">Home</button>
+          </Link>
+          <Link to="/new">
+            <button className="new-post-button">New Post</button>
+          </Link>
+          <Link to="/">
+            <button className="logout-button">Logout</button>
+          </Link>
+          </div>
+        </div>
+      )
+    } else
+      return null
   }
 }
 
-export default Nav;
+export default withRouter(Nav);
